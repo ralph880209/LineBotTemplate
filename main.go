91a24036178a3b3a1a1c85ce263cea31a5aa3cbc
ralph880209 <63499888+ralph880209@users.noreply.github.com>
2020-04-11 20:56:22 +0800

    Update main.go

--- main.go
+++ main.go
@@ -46,10 +46,10 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 
 	for _, event := range events {
 		switch event.Type {
-		case linebot.EventTypeBeacon:
+		case linebot.EventTypeBeacon.leave:
 			log.Println(" Beacon event....")
 			if b := event.Beacon; b != nil {
-				ret := fmt.Sprintln("Msg87:", string(b.DeviceMessage), " hwid:", b.Hwid)
+				ret := fmt.Sprintln("Msg:", string(b.DeviceMessage), " hwid:", b.Hwid)
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(ret)).Do(); err != nil {
 					log.Print(err)
 				}
