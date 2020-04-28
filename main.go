22c8fc69a28c0e3a595d1379b4e7efa197c35f7a
ralph880209 <63499888+ralph880209@users.noreply.github.com>
2020-04-28 21:35:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -51,7 +51,7 @@ func callbackHandler(w http.ResponseWriter, r *http.Request) {
 			case linebot.EventTypeBeacon:
 			log.Println(" Beacon event....")
 			if b := event.Beacon; b != nil {
-				ret := fmt.P("救護車接近提醒🚑🚑\r\n請放慢車速小心駕駛！")
+				ret := fmt.Print("救護車接近提醒🚑🚑\r\n請放慢車速小心駕駛！")
 				
 				if _, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(ret)).Do(); err != nil {
 					log.Print(err)
